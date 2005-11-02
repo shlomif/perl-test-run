fb90ca919a62870814dbf20558ca6d47020f8ddd
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-11-02 21:01:24 +0000

    Placed the helper objects in T::S::H::Obj::Structs.

--- modules/Test-Shlomif-Harness/lib/Test/Shlomif/Harness/Obj.pm
+++ modules/Test-Shlomif-Harness/lib/Test/Shlomif/Harness/Obj.pm
@@ -7,6 +7,7 @@ use Test::Shlomif::Harness::Straps;
 use Test::Shlomif::Harness::Output;
 use Test::Shlomif::Harness::Base;
 use Test::Shlomif::Harness::Assert;
+use Test::Shlomif::Harness::Obj::Structs;
 use Exporter;
 use Benchmark;
 use Config;
@@ -438,48 +439,6 @@ sub _tot_inc
     $self->_tot_add($field,1);
 }
 
-package Test::Shlomif::Harness::Obj::FailedObj;
-
-use vars qw(@ISA @fields %fields_map);
-
-@ISA = (qw(Test::Shlomif::Harness::Base));
-
-@fields = (qw(
-    canon
-    estat
-    failed
-    max
-    name
-    percent
-    wstat
-));
-
-%fields_map = (map { $_ => 1 } @fields);
-
-__PACKAGE__->mk_accessors(@fields);
-
-sub _initialize
-{
-    my $self = shift;
-    my (%args) = @_;
-
-    while (my ($k, $v) = each(%args))
-    {
-        if (exists($fields_map{$k}))
-        {
-            $self->set($k, $v);
-        }
-        else
-        {
-            die "Called with undefined field \"$k\"";
-        }
-    }
-}
-
-1;
-
-package Test::Shlomif::Harness::Obj;
-
 sub _create_failed_obj_instance
 {
     my $self = shift;
@@ -667,56 +626,6 @@ sub _process_passing_test
     $self->_tot_inc('good');
 }
 
-package Test::Shlomif::Harness::Obj::TestObj;
-
-use vars qw(@ISA @fields %fields_map);
-
-@ISA = (qw(Test::Shlomif::Harness::Base));
-
-@fields = (qw(
-    ok
-    next
-    max
-    failed
-    bonus
-    skipped
-    skip_reason
-    skip_all
-    ml
-));
-
-%fields_map = (map { $_ => 1 } @fields);
-
-__PACKAGE__->mk_accessors(@fields);
-
-sub _initialize
-{
-    my $self = shift;
-    my (%args) = @_;
-
-    while (my ($k, $v) = each(%args))
-    {
-        if (exists($fields_map{$k}))
-        {
-            $self->set($k, $v);
-        }
-        else
-        {
-            die "Called with undefined field \"$k\"";
-        }
-    }
-}
-
-sub add_to_failed
-{
-    my $self = shift;
-    push @{$self->failed()}, @_;
-}
-
-1;
-
-package Test::Shlomif::Harness::Obj;
-
 sub _create_test_obj_instance
 {
     my $self = shift;
@@ -795,87 +704,6 @@ sub _run_single_test
 
     $self->_recheck_dir_files();
 }
-
-package Test::Shlomif::Harness::Obj::TotObj;
-
-use vars qw(@ISA @fields %fields_map @counter_fields %counter_fields_map);
-
-@ISA = (qw(Test::Shlomif::Harness::Base));
-
-@counter_fields = (qw(
-    bad
-    bench
-    bonus
-    files
-    good
-    max
-    ok
-    skipped
-    sub_skipped
-    todo
-));
-
-@fields = (@counter_fields, 'tests');
-
-%fields_map = (map { $_ => 1 } @fields);
-%counter_fields_map = (map { $_ => 1 } @counter_fields);
-
-__PACKAGE__->mk_accessors(@fields);
-
-sub _pre_init
-{
-    my $self = shift;
-    foreach my $f (@counter_fields)
-    {
-        $self->set($f, 0);
-    }
-    return 0;
-}
-
-sub _initialize
-{
-    my $self = shift;
-    my (%args) = @_;
-
-    $self->_pre_init();
-
-    while (my ($k, $v) = each(%args))
-    {
-        if (exists($fields_map{$k}))
-        {
-            $self->set($k, $v);
-        }
-        else
-        {
-            die "Called with undefined field \"$k\"";
-        }
-    }
-}
-
-sub add
-{
-    my ($self, $field, $diff) = @_;
-    if (!exists($counter_fields_map{$field}))
-    {
-        die "Cannot add to field \"$field\"!";
-    }
-    $self->set($field, $self->get($field) + $diff);
-    return $self->get($field);
-}
-
-package Test::Shlomif::Harness::Obj;
-
-# sub _get_tot_counter_fields
-# {
-#     my $self = shift;
-#     return [];
-# }
-# 
-# sub _get_tot_counter_kv
-# {
-#     my $self = shift;
-#     return [map { $_ => 0 } @{$self->_get_tot_counter_fields()}];
-# }
 
 sub _get_tot_counter_tests
 {
