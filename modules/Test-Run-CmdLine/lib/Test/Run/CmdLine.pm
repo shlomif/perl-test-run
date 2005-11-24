5bbd249a13d5538bcda04c59dfeaa2488aa8eead
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-11-24 20:06:34 +0000

    Added the Test-Run-CmdLine module.

---
+++ modules/Test-Run-CmdLine/lib/Test/Run/CmdLine.pm
@@ -0,0 +1,72 @@
+package Test::Run::CmdLine;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Test::Run::CmdLine - The great new Test::Run::CmdLine!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Test::Run::CmdLine;
+
+    my $foo = Test::Run::CmdLine->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif@iglu.org.il> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-test-run-cmdline@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Run-CmdLine>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2005 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: bsd
+
+=cut
+
+1; # End of Test::Run::CmdLine
