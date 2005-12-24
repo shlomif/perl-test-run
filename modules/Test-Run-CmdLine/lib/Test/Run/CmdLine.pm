ed602d47f65f42b81af5c0786265c6b322933507
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2005-12-24 18:09:09 +0000

    Started added the handling of the Test::Harness environment variables. Added methods to enumerate the required parameters to Test::Run::Obj.
    Added the handling of the $ENV{HARNESS_FILELEAK_IN_DIR} variable which
    translates to 'Leaked_Dir' in Test::Run::Obj. Added tests and documentation
    for it.

--- modules/Test-Run-CmdLine/lib/Test/Run/CmdLine.pm
+++ modules/Test-Run-CmdLine/lib/Test/Run/CmdLine.pm
@@ -21,14 +21,14 @@ use vars (qw(@ISA));
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use Test::Run::CmdLine;
 
-    my $foo = Test::Run::CmdLine->new();
-    ...
+    my $tester = Test::Run::CmdLine->new(
+        'test_files' => ["t/one.t", "t/two.t"],
+    );
+
+    $tester->run();
+
 =cut
 
 __PACKAGE__->mk_accessors(qw(
@@ -46,9 +46,16 @@ sub _initialize
     $self->test_files($args{'test_files'});
 }
 
-=head2 $cmd_line->run()
+=head1 Interface Functions
 
-Actually runs the test on the command line.
+=head2 $tester = Test::Run::CmdLine->new('test_files' => \@test_files, ....);
+
+Initializes a new testing front end. C<test_files> is a named argument that
+contains the files to test.
+
+=head2 $tester->run()
+
+Actually runs the tests on the command line.
 
 TODO : Write more.
 
@@ -60,8 +67,11 @@ sub run
     my $driver_class = $self->driver_class();
     eval "require $driver_class";
 
+    my $back_end_args = $self->get_backend_args();
+
     my $driver = $driver_class->new(
         'test_files' => $self->test_files(),
+        @$back_end_args,
     );
 
     return $driver->runtests();
@@ -90,6 +100,73 @@ sub _set_driver_class
     }
     $self->driver_class($driver_class);
 }
+
+=head1 Environment Variables
+
+The following environment variables (C<%ENV>) affect the behaviour of 
+Test::Run::CmdLine:
+
+=head2 HARNESS_FILELEAK_IN_DIR
+
+This variable points to a directory that will be monitored. After each
+test file, the module will check if new files appeared in the direcotry
+and report them. 
+    
+It is advisable to give an absolute path here. If it is relative, it would
+be relative to the current working directory when C<$tester-E<gt>run()> was
+called.
+
+=head1 Internal Functions
+
+=head2 my $args_array_ref = $tester->get_backend_args()
+
+Calculate and retrieve the arguments for the backend class (that inherits
+from L<Test::Run::Obj>) as a single array reference. Currently it appends
+the arguments of get_backend_env_args() to that of get_backend_init_args().
+
+=cut
+
+sub get_backend_args
+{
+    my $self = shift;
+
+    my $env_var_args = $self->get_backend_env_args();
+
+    my $init_args = $self->get_backend_init_args();
+
+    return [@$env_var_args, @$init_args,];
+}
+
+=head2 my $args_array_ref = $tester->get_backend_env_args()
+
+Calculate and return the arguments for the backend class, that originated
+from the environment (%ENV).
+
+=cut
+
+sub get_backend_env_args
+{
+    my $self = shift;
+    my @args;
+    if (exists($ENV{HARNESS_FILELEAK_IN_DIR}))
+    {
+        push @args, ('Leaked_Dir' => $ENV{HARNESS_FILELEAK_IN_DIR});
+    }
+    return \@args;
+}
+
+=head2 my $args_array_ref = $tester->get_backend_init_args()
+
+Calculate and return the arguments for the backend class, that originated
+from the arguments passed to the (front-end) object from its constructor.
+
+=cut
+
+sub get_backend_init_args
+{
+    return [];
+}
+
 
 =head1 AUTHOR
 
--- modules/Test-Run-CmdLine/t/03-prove.t
+++ modules/Test-Run-CmdLine/t/03-prove.t
@@ -2,16 +2,22 @@
 
 use strict;
 
-use Test::More tests => 1;
+use Test::More tests => 2;
 use File::Spec;
+use File::Path;
+use Config;
 
 my $blib = File::Spec->catfile( File::Spec->curdir, "blib" );
 my $lib = File::Spec->catfile( $blib, "lib" );
 my $runprove = File::Spec->catfile( $blib, "script", "runprove" );
-my $test_file = File::Spec->catfile("t", "sample-tests", "one-ok.t");
+my $sample_tests_dir = File::Spec->catfile("t", "sample-tests");
+my $test_file = File::Spec->catfile($sample_tests_dir, "one-ok.t");
+my $leaked_files_dir = File::Spec->catfile($sample_tests_dir, "leaked-files-dir");
+my $leaked_file = File::Spec->catfile($leaked_files_dir, "hello.txt");
+my $leak_test_file = File::Spec->catfile($sample_tests_dir, "leak-file.t");
 {
     local $ENV{'TEST_HARNESS_DRIVER'};
-    local $ENV{'PERL5LIB'} = $lib;
+    local $ENV{'PERL5LIB'} = $lib.$Config{'path_sep'}.$ENV{'PERL5LIB'};
     
     {
         my $results = qx{$runprove $test_file};
@@ -19,6 +25,22 @@ my $test_file = File::Spec->catfile("t", "sample-tests", "one-ok.t");
         # TEST
         ok (($results =~ m/All tests successful\./), 
             "Good results from runprove");
+    }
+
+    {
+        mkdir($leaked_files_dir, 0777);
+        open O, ">", $leaked_file;
+        print O "This is the file hello.txt";
+        close(O);
+
+        local $ENV{'HARNESS_FILELEAK_IN_DIR'} = $leaked_files_dir;
+
+        my $results = qx{$runprove $leak_test_file $test_file};
+
+        # TEST
+        ok (($results =~ m/\nLEAKED FILES: new-file.txt\n/),
+            "Checking for files that were leaked");
+        rmtree([$leaked_files_dir], 0, 0);
     }
 }
 1;
