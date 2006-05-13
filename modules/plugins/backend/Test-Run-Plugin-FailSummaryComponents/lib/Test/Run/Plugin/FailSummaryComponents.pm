05ce113bc2624bd1dc4db1bb2f32e3386c9a4738
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-05-13 10:31:56 +0000

    Added POD documentation.

--- modules/plugins/backend/Test-Run-Plugin-FailSummaryComponents/lib/Test/Run/Plugin/FailSummaryComponents.pm
+++ modules/plugins/backend/Test-Run-Plugin-FailSummaryComponents/lib/Test/Run/Plugin/FailSummaryComponents.pm
@@ -38,6 +38,43 @@ sub _get_simple_params
     ];
 }
 
+
+=head1 SYNOPSIS
+
+    package MyTestRun;
+    
+    use vars qw(@ISA);
+
+    @ISA = (qw(Test::Run::Plugin::FailSummaryComponents Test::Run::Obj));
+
+    my $tester = MyTestRun->new(
+        test_files => 
+        [
+            "t/sample-tests/one-ok.t",
+            "t/sample-tests/several-oks.t"
+        ],
+        );
+
+    $tester->runtests();
+
+=head1 EXTRA PARAMETERS TO NEW
+
+We accept three new named parameters to the new constructor:
+
+=head2 failsumm_remove_test_scripts_number
+
+If set, removes the $N-out-of-$N test scripts number from the failure line.
+
+=head2 failsumm_remove_test_scripts_percent
+
+If set, removes the percent of the test scripts that failed.
+
+=head2 failsumm_remove_subtests_percent
+
+If set, removes the percent of the subtests that failed.
+
+=cut
+
 sub _get_fail_test_scripts_string
 {
     my $self = shift;
@@ -80,44 +117,6 @@ sub _get_sub_percent_msg
         );
 }
 
-=head1 SYNOPSIS
-
-    package MyTestRun;
-    
-    use vars qw(@ISA);
-
-    @ISA = (qw(Test::Run::Plugin::FailSummaryComponents Test::Run::Obj));
-
-    my $tester = MyTestRun->new(
-        test_files => 
-        [
-            "t/sample-tests/one-ok.t",
-            "t/sample-tests/several-oks.t"
-        ],
-        );
-
-    $tester->runtests();
-
-=head1 EXTRA PARAMETERS TO NEW
-
-We accept two new named parameters to the new constructor:
-
-=head2 summary_color_success
-
-This is the color string for coloring the success line. The string itself
-conforms to the one specified in L<Term::ANSIColor>.
-
-=head2 summary_color_failure
-
-This is the color string for coloring the summary line in case of
-failure. The string itself conforms to the one specified 
-in L<Term::ANSIColor>.
-
-=head1 FUNCTIONS
-
-=cut
-
-1;
 
 =head1 AUTHOR
 
@@ -126,7 +125,7 @@ Shlomi Fish, C<< <shlomif@iglu.org.il> >>
 =head1 BUGS
 
 Please report any bugs or feature requests to
-C<bug-test-run-plugin-colorsummary@rt.cpan.org>, or through the web interface at
+C<bug-test-run-plugin-failsummarycomponents@rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Run-Plugin-FailSummaryComponents>.
 I will be notified, and then you'll automatically be notified of progress on
 your bug as I make changes.
@@ -139,7 +138,7 @@ L<Test::Run::Obj>, L<Test::Run::CmdLine::Plugin::FailSummaryComponents>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2005 Shlomi Fish, all rights reserved.
+Copyright 2006 Shlomi Fish, all rights reserved.
 
 This program is released under the MIT X11 License.
 
