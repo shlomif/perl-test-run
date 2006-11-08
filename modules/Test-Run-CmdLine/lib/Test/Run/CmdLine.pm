ef0ac9865e26800b7e34918b87f8a2f0436dd2f6
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-11-08 08:04:08 +0000

    * Added some links to the POD, most of which are inserted by new versions of Module-Starter.

--- modules/Test-Run-CmdLine/lib/Test/Run/CmdLine.pm
+++ modules/Test-Run-CmdLine/lib/Test/Run/CmdLine.pm
@@ -304,7 +304,7 @@ sub add_to_backend_plugins
     unshift @{$self->backend_plugins()}, $plugin;
 }
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Shlomi Fish, C<< <shlomif@iglu.org.il> >>
 
@@ -315,6 +315,41 @@ C<bug-test-run-cmdline@rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Run-CmdLine>.
 I will be notified, and then you'll automatically be notified of progress on
 your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::Run::CmdLine
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test::Run::CmdLine>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test::Run::CmdLine>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test::Run::CmdLine>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test::Run::CmdLine/>
+
+=back
+
+=head1 SOURCE AVAILABILITY
+
+The latest source of Test::Run::CmdLine is available from the Test::Run
+BerliOS Subversion repository:
+
+L<https://svn.berlios.de/svnroot/repos/web-cpan/Test-Harness-NG/>
 
 =head1 ACKNOWLEDGEMENTS
 
