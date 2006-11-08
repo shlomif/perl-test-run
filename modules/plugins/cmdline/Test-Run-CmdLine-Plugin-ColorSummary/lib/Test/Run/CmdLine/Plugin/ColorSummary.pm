64a1faeb515d9ea200469de1b71d7b852a257593
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-11-08 15:05:39 +0000

    Added the links to the POD, and bumped the version number.

--- modules/plugins/cmdline/Test-Run-CmdLine-Plugin-ColorSummary/lib/Test/Run/CmdLine/Plugin/ColorSummary.pm
+++ modules/plugins/cmdline/Test-Run-CmdLine-Plugin-ColorSummary/lib/Test/Run/CmdLine/Plugin/ColorSummary.pm
@@ -11,7 +11,7 @@ Test::Run::CmdLine::Plugin::ColorSummary - Color the summary in Test::Run::CmdLi
 
 =cut
 
-our $VERSION = '0.0100_01';
+our $VERSION = '0.0101';
 
 sub _initialize
 {
@@ -67,6 +67,41 @@ C<bug-test-run-cmdline-plugin-colorsummary@rt.cpan.org>, or through the web inte
 L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Run-CmdLine-Plugin-ColorSummary>.
 I will be notified, and then you'll automatically be notified of progress on
 your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::Run::CmdLine::Plugin::ColorSummary
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test::Run::CmdLine::Plugin::ColorSummary>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test::Run::CmdLine::Plugin::ColorSummary>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test::Run::CmdLine::Plugin::ColorSummary>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test::Run::CmdLine::Plugin::ColorSummary/>
+
+=back
+
+=head1 SOURCE AVAILABILITY
+
+The latest source of Test::Run::CmdLine::Plugin::ColorSummary is available from the 
+Test::Run BerliOS Subversion repository:
+
+L<https://svn.berlios.de/svnroot/repos/web-cpan/Test-Harness-NG/>
 
 =head1 SEE ALSO
 
