8ec8e74aa4bba587fce7ca199906e90c009d6a41
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-02-16 11:09:03 +0000

    Commited the boilerplate for the AlternateInterpreters plugin.

---
+++ modules/plugins/backend/Test-Run-Plugin-AlternateInterpreters/lib/Test/Run/Plugin/AlternateInterpreters.pm
@@ -0,0 +1,100 @@
+package Test::Run::Plugin::AlternateInterpreters;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Test::Run::Plugin::AlternateInterpreters - The great new Test::Run::Plugin::AlternateInterpreters!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Test::Run::Plugin::AlternateInterpreters;
+
+    my $foo = Test::Run::Plugin::AlternateInterpreters->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-test-run-plugin-alternateinterpreters at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test::Run::Plugin::AlternateInterpreters>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::Run::Plugin::AlternateInterpreters
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test::Run::Plugin::AlternateInterpreters>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test::Run::Plugin::AlternateInterpreters>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test::Run::Plugin::AlternateInterpreters>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test::Run::Plugin::AlternateInterpreters>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: bsd
+
+=cut
+
+1; # End of Test::Run::Plugin::AlternateInterpreters
