1cfd5c0aa2bcb73c23bf3b6d195e7d2097020dfb
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-03-13 14:24:30 +0000

    Extracted TotObj to its own module.

--- modules/Test-Shlomif-Harness/lib/Test/Run/Obj/Structs.pm
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Obj/Structs.pm
@@ -7,60 +7,8 @@ use Carp;
 
 use Test::Run::Obj::FailedObj;
 use Test::Run::Obj::TestObj;
-
-package Test::Run::Obj::TotObj;
-
-use vars qw(@ISA @fields @counter_fields %counter_fields_map);
-
-@ISA = (qw(Test::Run::Base::Struct));
-
-@counter_fields = (qw(
-    bad
-    bench
-    bonus
-    files
-    good
-    max
-    ok
-    skipped
-    sub_skipped
-    todo
-));
-
-@fields = (@counter_fields, 'tests');
-
-sub _get_fields
-{
-    return [@fields];
-}
-
-%counter_fields_map = (map { $_ => 1 } @counter_fields);
-
-__PACKAGE__->mk_accessors(@fields);
-
-sub _pre_init
-{
-    my $self = shift;
-    foreach my $f (@counter_fields)
-    {
-        $self->set($f, 0);
-    }
-    return 0;
-}
-
-sub add
-{
-    my ($self, $field, $diff) = @_;
-    if (!exists($counter_fields_map{$field}))
-    {
-        Carp::confess "Cannot add to field \"$field\"!";
-    }
-    $self->set($field, $self->get($field) + $diff);
-    return $self->get($field);
-}
-
-1;
-
+use Test::Run::Obj::TotObj;
+    
 package Test::Run::Obj::CanonFailedObj;
 
 use vars qw(@ISA @fields);
---
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Obj/TotObj.pm
@@ -0,0 +1,87 @@
+package Test::Run::Obj::TotObj;
+
+=head1 NAME
+
+Test::Run::Obj::TotObj - totals encountered for the entire Test::Run session
+
+=head1 DESCRIPTION
+
+Inherits from L<Test::Run::Base::Struct>.
+
+=head1 METHODS
+
+=cut
+
+use vars qw(@fields @counter_fields %counter_fields_map);
+
+use base qw(Test::Run::Base::Struct);
+
+@counter_fields = (qw(
+    bad
+    bench
+    bonus
+    files
+    good
+    max
+    ok
+    skipped
+    sub_skipped
+    todo
+));
+
+@fields = (@counter_fields, 'tests');
+
+sub _get_fields
+{
+    return [@fields];
+}
+
+%counter_fields_map = (map { $_ => 1 } @counter_fields);
+
+__PACKAGE__->mk_accessors(@fields);
+
+sub _pre_init
+{
+    my $self = shift;
+    foreach my $f (@counter_fields)
+    {
+        $self->set($f, 0);
+    }
+    return 0;
+}
+
+=head2 $self->add($field, $diff)
+
+Adds the difference $diff to the slot $field, assuming it is a counter field.
+
+=cut
+
+sub add
+{
+    my ($self, $field, $diff) = @_;
+    if (!exists($counter_fields_map{$field}))
+    {
+        Carp::confess "Cannot add to field \"$field\"!";
+    }
+    $self->set($field, $self->get($field) + $diff);
+    return $self->get($field);
+}
+
+1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<Test::Run::Base::Struct>, L<Test::Run::Obj>, L<Test::Run::Core>
+
+=head1 LICENSE
+
+This file is freely distributable under the MIT X11 license.
+
+=head1 AUTHOR
+
+Shlomi Fish, L<http://www.shlomifish.org/>.
+
+=cut
+
