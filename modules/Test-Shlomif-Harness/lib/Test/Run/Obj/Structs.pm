5ce180352909c13c62e5387fe7a7c432c28c7477
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-03-14 12:26:21 +0000

    Test::Run (core) - placed Test::Run::Straps::StrapsTotalsObj in its own .pm module file.

--- modules/Test-Shlomif-Harness/lib/Test/Run/Obj/Structs.pm
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Obj/Structs.pm
@@ -9,88 +9,8 @@ use Test::Run::Obj::FailedObj;
 use Test::Run::Obj::TestObj;
 use Test::Run::Obj::TotObj;
 use Test::Run::Obj::CanonFailedObj;
+use Test::Run::Straps::StrapsTotalsObj;
 
-
-package Test::Run::Straps::StrapsTotalsObj;
-
-use vars qw(@ISA @fields);
-
-@ISA = (qw(Test::Run::Base::Struct));
-
-@fields = (qw(
-    bonus
-    details
-    exit
-    max
-    ok
-    passing
-    seen
-    skip
-    skip_all
-    skip_reason
-    todo
-    wait
-));
-
-sub _get_fields
-{
-    return [@fields];
-}
-
-__PACKAGE__->mk_accessors(@fields);
-
-sub _calc_passing
-{
-    my $self = shift;
-    return 
-    (
-        ($self->max() == 0 &&  defined $self->skip_all()) ||
-        (
-            $self->max() && $self->seen() &&
-            $self->max() == $self->seen() &&
-            $self->max() == $self->ok()
-        )
-    );
-}
-
-sub determine_passing
-{
-    my $self = shift;
-    $self->passing($self->_calc_passing() ? 1 : 0);
-}
-
-sub update_skip_reason
-{
-    my $self = shift;
-    my $detail = shift;
-
-    if( $detail->type() eq 'skip' )
-    {
-        my $reason = $detail->reason();
-        if (!defined($self->skip_reason()))
-        {
-            $self->skip_reason($reason);
-        }
-        elsif ($self->skip_reason() ne $reason)
-        {
-            $self->skip_reason('various reasons');
-        }
-    }
-}
-
-=head2 $self->last_detail()
-
-Returns the last detail.
-
-=cut
-
-sub last_detail
-{
-    my $self = shift;
-    return $self->details()->[-1];
-}
-
-1;
 
 package Test::Run::Straps::StrapsDetailsObj;
 
---
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Straps/StrapsTotalsObj.pm
@@ -0,0 +1,126 @@
+package Test::Run::Straps::StrapsTotalsObj;
+
+=head1 NAME
+
+Test::Run::Straps::StrapsTotalsObj - an object representing the totals of the
+straps class.
+
+=head1 METHODS
+
+=cut
+
+use vars qw(@fields);
+
+use base 'Test::Run::Base::Struct';
+
+
+@fields = (qw(
+    bonus
+    details
+    exit
+    max
+    ok
+    passing
+    seen
+    skip
+    skip_all
+    skip_reason
+    todo
+    wait
+));
+
+sub _get_fields
+{
+    return [@fields];
+}
+
+__PACKAGE__->mk_accessors(@fields);
+
+=head2 $self->_calc_passing()
+
+Calculates whether the test file has passed.
+
+=cut
+
+sub _calc_passing
+{
+    my $self = shift;
+    return 
+    (
+        ($self->max() == 0 &&  defined $self->skip_all()) ||
+        (
+            $self->max() && $self->seen() &&
+            $self->max() == $self->seen() &&
+            $self->max() == $self->ok()
+        )
+    );
+}
+
+=head2 $self->determine_passing()
+
+Calculates whether the test file has passed and caches it in the passing()
+slot.
+
+=cut
+
+sub determine_passing
+{
+    my $self = shift;
+    $self->passing($self->_calc_passing() ? 1 : 0);
+}
+
+=head2 $self->update_skip_reason($detail)
+
+Updates the skip reason according to the detail $detail.
+
+=cut
+
+sub update_skip_reason
+{
+    my $self = shift;
+    my $detail = shift;
+
+    if( $detail->type() eq 'skip' )
+    {
+        my $reason = $detail->reason();
+        if (!defined($self->skip_reason()))
+        {
+            $self->skip_reason($reason);
+        }
+        elsif ($self->skip_reason() ne $reason)
+        {
+            $self->skip_reason('various reasons');
+        }
+    }
+}
+
+=head2 $self->last_detail()
+
+Returns the last detail.
+
+=cut
+
+sub last_detail
+{
+    my $self = shift;
+    return $self->details()->[-1];
+}
+
+1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<Test::Run::Base::Struct>, L<Test::Run::Obj>, L<Test::Run::Core>
+
+=head1 LICENSE
+
+This file is freely distributable under the same terms as Perl 5 itself.
+
+=head1 AUTHOR
+
+Shlomi Fish, L<http://www.shlomifish.org/>.
+
+=cut
+
