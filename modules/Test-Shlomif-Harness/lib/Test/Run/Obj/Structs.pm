a1623f0f18959eecad584d00e40a2c330ebc5cf4
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-03-14 12:51:14 +0000

    Test::Run (core) - Placed StrapsDetailsObj in its own .pm file.

--- modules/Test-Shlomif-Harness/lib/Test/Run/Obj/Structs.pm
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Obj/Structs.pm
@@ -10,40 +10,6 @@ use Test::Run::Obj::TestObj;
 use Test::Run::Obj::TotObj;
 use Test::Run::Obj::CanonFailedObj;
 use Test::Run::Straps::StrapsTotalsObj;
-
-
-package Test::Run::Straps::StrapsDetailsObj;
-
-use vars qw(@ISA @fields);
-
-@ISA = (qw(Test::Run::Base::Struct));
-
-@fields = (qw(
-    actual_ok
-    diagnostics
-    name
-    ok
-    reason
-    type
-));
-
-sub _get_fields
-{
-    return [@fields];
-}
-
-sub _pre_init
-{
-    my $self = shift;
-    $self->diagnostics("");
-}
-__PACKAGE__->mk_accessors(@fields);
-
-sub append_to_diag
-{
-    my ($self, $text) = @_;
-    $self->diagnostics($self->diagnostics().$text);
-}
+use Test::Run::Straps::StrapsDetailsObj;
 
 1;
-
---
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Straps/StrapsDetailsObj.pm
@@ -0,0 +1,71 @@
+package Test::Run::Straps::StrapsDetailsObj;
+
+=head1 NAME
+
+Test::Run::Straps::StrapsDetailsObj - a struct representing the details of
+the straps class.
+
+=head1 DESCRIPTIONS
+
+Inherits from Test::Run::Base::Struct.
+
+=head1 METHODS
+
+=cut
+
+use vars qw(@fields);
+
+use base 'Test::Run::Base::Struct';
+
+@fields = (qw(
+    actual_ok
+    diagnostics
+    name
+    ok
+    reason
+    type
+));
+
+sub _get_fields
+{
+    return [@fields];
+}
+
+sub _pre_init
+{
+    my $self = shift;
+    $self->diagnostics("");
+}
+
+__PACKAGE__->mk_accessors(@fields);
+
+=head2 $self->append_to_diag($text)
+
+Appends $text to the diagnostics.
+
+=cut
+
+sub append_to_diag
+{
+    my ($self, $text) = @_;
+    $self->diagnostics($self->diagnostics().$text);
+}
+
+1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<Test::Run::Base::Struct>, L<Test::Run::Obj>, L<Test::Run::Core>
+
+=head1 LICENSE
+
+This file is freely distributable under the terms of the MIT X11 License.
+
+=head1 AUTHOR
+
+Shlomi Fish, L<http://www.shlomifish.org/>.
+
+=cut
+
