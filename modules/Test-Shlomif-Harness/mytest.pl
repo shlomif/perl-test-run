2caacfd4f0aed9c479a6283a73d2be8d9d3133e1
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-04-20 16:32:55 +0000

    Adapted to the new one.

--- modules/Test-Shlomif-Harness/mytest.pl
+++ modules/Test-Shlomif-Harness/mytest.pl
@@ -7,10 +7,12 @@ use Test::Run::Obj;
 
 my $tester = 
     Test::Run::Obj->new(
-        # 'test_files' => ["t/sample-tests/simple_fail"]
-        'test_files' => ["t/sample-tests/head_end"],
-        'Verbose' => 1,
-        'Debug' => 1,
+        {
+            # 'test_files' => ["t/sample-tests/simple_fail"]
+            'test_files' => ["t/sample-tests/head_end"],
+            'Verbose' => 1,
+            'Debug' => 1,
+        }
     );
 $tester->runtests();
 
