004eabcdad065aab2895e396f04254ce24e6a2e5
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-04-25 08:54:39 +0000

    Added the license blurb to check-manifest.

--- modules/Test-Shlomif-Harness/check-manifest.sh
+++ modules/Test-Shlomif-Harness/check-manifest.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This file is licensed under the MIT X11 License:
+# http://www.opensource.org/licenses/mit-license.php
 cat file-list.txt | grep -v '/$' | sort | diff -u MANIFEST - |
     grep -vP '^\+bin/prove$' | grep -vP '^\+check-manifest\.sh$' |
     grep -vF META.yml | grep -v '^ ' | grep -vP '^(\+\+\+|\-\-\-|@@)'
