cf0cd254e7d1ae6f8c8edfd23047d278283da4cc
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-04-26 07:57:19 +0000

    Test::Run: more de-GPLing.

--- modules/Test-Shlomif-Harness/lib/Test/Run/Straps/StrapsTotalsObj.pm
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Straps/StrapsTotalsObj.pm
@@ -3,6 +3,15 @@ package Test::Run::Straps::StrapsTotalsObj;
 use strict;
 use warnings;
 
+=head1 NAME
+
+Test::Run::Straps::StrapsTotalsObj - an object representing the totals of the
+straps class.
+
+=head1 FIELDS
+
+=cut
+
 use base 'Test::Run::Straps::StrapsTotalsObj_GplArt';
 
 use vars qw(@fields);
@@ -31,6 +40,96 @@ sub _get_fields
 }
 
 __PACKAGE__->mk_accessors(@fields);
+
+=head1 METHODS
+
+=head2 $self->_calc_passing()
+
+Calculates whether the test file has passed.
+
+=cut
+
+sub _is_skip_all
+{
+    my $self = shift;
+    
+    return (($self->max() == 0) && defined($self->skip_all()));
+}
+
+sub _is_all_tests_passed
+{
+    my $self = shift;
+
+    return
+    (
+        $self->max && $self->seen
+        && ($self->max == $self->seen)
+        && ($self->max == $self->ok)
+    );
+}
+
+sub _calc_passing
+{
+    my $self = shift;
+
+    return ($self->_is_skip_all() || $self->_is_all_tests_passed());
+}
+
+=head2 $self->bonus()
+
+Number of TODO tests that unexpectedly passed.
+
+=head2 $self->details()
+
+An array containing the details of the individual checks in the test file.
+
+=head2 $self->exit()
+
+The exit code of the test script.
+
+=head2 $self->filename()
+
+The filename of the test script.
+
+=head2 $self->max()
+
+The number of planned tests.
+
+=head2 $self-ok()
+
+The number of tests that passed.
+
+=head2 $self->passing()
+
+A boolean value that indicates whether the entire test script is considered
+a success or not.
+
+=head2 $self->seen()
+
+The number of tests that were actually run.
+
+=head2 $self->skip()
+
+The number of skipped tests.
+
+=head2 $self->skip_all()
+
+This field will contain the reason for why the entire test script was skipped,
+in cases when it was.
+
+=head2 $self->skip_reason()
+
+The skip reason for the last skipped test that specified such a reason.
+
+=head2 $self->todo()
+
+The number of "Todo" tests that were encountered.
+
+=head2 $self->wait()
+
+The wait code of the test script.
+
+=cut
 
 =head1 LICENSE
 
--- modules/Test-Shlomif-Harness/lib/Test/Run/Straps/StrapsTotalsObj_GplArt.pm
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Straps/StrapsTotalsObj_GplArt.pm
@@ -1,101 +1,15 @@
 package Test::Run::Straps::StrapsTotalsObj_GplArt;
 
-=head1 NAME
-
-Test::Run::Straps::StrapsTotalsObj - an object representing the totals of the
-straps class.
-
-=head1 FIELDS
-
-=cut
-
 use base 'Test::Run::Base::Struct';
 
 use Test::Run::Assert;
 
 use Test::Run::Straps::StrapsDetailsObj;
 
-=head2 $self->bonus()
-
-Number of TODO tests that unexpectedly passed.
-
-=head2 $self->details()
-
-An array containing the details of the individual checks in the test file.
-
-=head2 $self->exit()
-
-The exit code of the test script.
-
-=head2 $self->filename()
-
-The filename of the test script.
-
-=head2 $self->max()
-
-The number of planned tests.
-
-=head2 $self-ok()
-
-The number of tests that passed.
-
-=head2 $self->passing()
-
-A boolean value that indicates whether the entire test script is considered
-a success or not.
-
-=head2 $self->seen()
-
-The number of tests that were actually run.
-
-=head2 $self->skip()
-
-The number of skipped tests.
-
-=head2 $self->skip_all()
-
-This field will contain the reason for why the entire test script was skipped,
-in cases when it was.
-
-=head2 $self->skip_reason()
-
-The skip reason for the last skipped test that specified such a reason.
-
-=head2 $self->todo()
-
-The number of "Todo" tests that were encountered.
-
-=head2 $self->wait()
-
-The wait code of the test script.
-
-=cut
 
 sub _def_or_blank {
     return $_[0] if defined $_[0];
     return "";
-}
-
-=head1 METHODS
-
-=head2 $self->_calc_passing()
-
-Calculates whether the test file has passed.
-
-=cut
-
-sub _calc_passing
-{
-    my $self = shift;
-    return 
-    (
-        ($self->max() == 0 &&  defined $self->skip_all()) ||
-        (
-            $self->max() && $self->seen() &&
-            $self->max() == $self->seen() &&
-            $self->max() == $self->ok()
-        )
-    );
 }
 
 =head2 $self->determine_passing()
