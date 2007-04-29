4d25c758ffc8691100b2f0cd921cd53be44a5197
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-04-29 13:16:11 +0000

    Test::Run: licensed Test::Run::Iface under the MIT X11 license.

--- modules/Test-Shlomif-Harness/lib/Test/Run/Iface.pm
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Iface.pm
@@ -7,3 +7,14 @@ use base 'Test::Run::Base';
 
 1;
 
+=head1 NAME
+
+Test::Run::Iface  
+
+=head1 LICENSE
+
+This file is licensed under the MIT X11 License:
+
+http://www.opensource.org/licenses/mit-license.php
+
+=cut
