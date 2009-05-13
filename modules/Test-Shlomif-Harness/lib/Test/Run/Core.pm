8382268b6d22a57274006b7ab55cdd73491c3f34
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2009-05-13 20:53:14 +0000

    Bumped the version number.

--- modules/Test-Shlomif-Harness/lib/Test/Run/Core.pm
+++ modules/Test-Shlomif-Harness/lib/Test/Run/Core.pm
@@ -29,11 +29,11 @@ Test::Run::Core - Base class to run standard TAP scripts.
 
 =head1 VERSION
 
-Version 0.0118
+Version 0.0119
 
 =cut
 
-$VERSION = '0.0118';
+$VERSION = '0.0119';
 
 $ENV{HARNESS_ACTIVE} = 1;
 $ENV{HARNESS_NG_VERSION} = $VERSION;
